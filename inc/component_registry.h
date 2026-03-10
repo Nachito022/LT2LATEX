cf9249804cf630178597181a508a2838ff0a7270
Nachito022 <nachoferre22@gmail.com>
2026-03-10 14:18:59 -0300

    added more components

--- inc/component_registry.h
+++ inc/component_registry.h
@@ -15,6 +15,7 @@ static const ComponentMap COMPONENT_MAP[] = {
     { "ind",            "inductor" },
     { "ind2",           "cute inductor" },
     { "pot",            "pR" },
+    { "polcap",         "capacitor" },
 
     /* Diodes */
     { "diode",          "diode" },
@@ -22,12 +23,18 @@ static const ComponentMap COMPONENT_MAP[] = {
     { "schottky",       "schottky" },
     { "led",            "led" },
     { "photo",          "photodiode" },
+    { "varactor",       "varactor" },
+    { "TVSdiode",       "tvs diode" },
 
     /* Transistors - BJT */
     { "npn",            "npn" },
     { "pnp",            "pnp" },
     { "npn2",           "npn" },
     { "pnp2",           "pnp" },
+    { "lpnp",           "pnp" },
+    { "npn3",           "npn" },
+    { "npn4",           "npn" },
+    { "pnp4",           "pnp" },
 
     /* Transistors - MOSFET */
     { "nmos",           "nmos" },
@@ -48,22 +55,58 @@ static const ComponentMap COMPONENT_MAP[] = {
     { "f",              "controlled current source" },
     { "g",              "controlled current source" },
     { "h",              "controlled voltage source" },
+    { "e2",             "controlled voltage source" },
+    { "g2",             "controlled current source" },
+    { "bv",             "controlled voltage source" },
 
     /* Opamps & Comparators */
     { "opamp",          "op amp" },
     { "opamp2",         "op amp" },
     { "comparator",     "op amp" },
+    { "UniversalOpAmp", "op amp" },
+    
 
     /* Switches */
     { "sw",             "nos" },
     { "csw",            "ncs" },
 
+    /* Transmission lines */
+    { "tline",          "transmission line" },
+    { "ltline",         "transmission line" },
+
+    /* Loads */
+    { "load",           "load" },
+    { "load2",          "load" },
+
+    /* Thermal */
+    { "SOAtherm_PCB",       "thermal" },
+    { "SOAtherm_HeatSink",  "thermal" },
+    { "SOAtherm_NMOS",      "thermal" },
+
+    /* Automotive */
+    { "ISO16750_2",     "voltage source" },
+    { "ISO7637_2",      "voltage source" },
+
+    /* FRA */
+    { "fra",            "fra" },
+    { "fraprobe",       "fra probe" },
+
     /* Misc */
     { "xtal",           "crystal" },
+    { "mesfet",         "mesfet" },
     { "transformer",    "transformer" },
     { "transformer2",   "ideal transformer" },
     { "fuse",           "fuse" },
     { "antenna",        "antenna" },
+    { "FerriteBead",    "ferrite bead" },
+    { "FerriteBead2",   "ferrite bead" },
+
+
+    /* Specific models - map to their generic type */
+    { "TIP41C",         "npn" },
+    { "TIP42C",         "pnp" },
+    { "TL081",          "op amp" },
+
 };
 
 static const int COMPONENT_MAP_SIZE =
